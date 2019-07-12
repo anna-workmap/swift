589afe1561273216d562ea62b90cac93108e79e6
Anhi <anhi@Anhis-MacBook-Air.local>
2019-07-12 20:53:21 +0700

    add ImRich Project

---
+++ I AM RICH/I AM RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Anhi on 5/24/19.
+//  Copyright © 2019 ANNA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
